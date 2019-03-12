0e5e4731d7f5f38012cdd02cd2c1145b84a2900c
Anush Shrestha <anush4270@gmail.com>
2019-03-12 18:34:25 -0500

    Create ui.R
    - Includes the basic layout of the app
    - Initialize default value for visualization

---
+++ ui.R
@@ -0,0 +1,22 @@
+library(shiny)
+
+#Define UI for app
+ui <- fluidPage(
+  # App title
+  titlePanel("Network Intellisense"),
+  
+  # Sidebar layour for input values
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(inputId = "bins","Please Select Bin Size: ",
+                  min = 1, max=50, value= 30)
+    ),
+    
+    # Main panel for displaying output
+    mainPanel(
+      #Output : different visualizations that implements different algorithms from network theory  
+      plotOutput(outputId = "distPlot")
+    )
+  )
+)
+
